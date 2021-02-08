fb0c03abc85eb2d080d1213e2cd11884a0109d72
Joshua M. Clulow <josh@sysmgr.org>
2021-02-07 21:21:54 -0800

    XXX basic in-kernel version access is OK

--- usr/src/uts/common/sys/fs/p9fs_impl.h
+++ usr/src/uts/common/sys/fs/p9fs_impl.h
@@ -16,6 +16,10 @@
 #ifndef _SYS_FS_P9FS_IMPL_H
 #define	_SYS_FS_P9FS_IMPL_H
 
+#include <sys/ddi.h>
+#include <sys/sunddi.h>
+#include <sys/sunldi.h>
+
 /*
  * XXX p9fs
  */
@@ -25,6 +29,22 @@ extern "C" {
 #endif
 
 extern const struct fs_operation_def p9fs_vnodeops_template[];
+extern struct vnodeops *p9fs_vnodeops;
+
+typedef struct p9fs {
+	vnode_t *p9_root;
+} p9fs_t;
+
+typedef struct p9fs_session {
+	ldi_handle_t p9s_ldi;
+} p9fs_session_t;
+
+typedef struct p9fs_req {
+} p9fs_req_t;
+
+
+extern int p9fs_session_init(p9fs_session_t **p9s, ldi_handle_t lh);
+extern void p9fs_session_fini(p9fs_session_t *p9s);
 
 #ifdef __cplusplus
 }
